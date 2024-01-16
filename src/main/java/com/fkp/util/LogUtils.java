727f3f4cb8132b96fd2d2d86200ce16a83ab1620
fengkunpeng <fkp123456>
2024-01-16 17:55:57 +0800

    1.日志打印封装工具类后打印调用的类名和行号

---
+++ src/main/java/com/fkp/util/LogUtils.java
@@ -0,0 +1,35 @@
+package com.fkp.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author fengkunpeng
+ * @version 1.0
+ * @description
+ * @date 2024/1/15 15:16
+ */
+public class LogUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger("javaStd");
+
+    public static void error(String stringFormat, Object... args){
+        logger.error(buildStringFormat(stringFormat), Thread.currentThread().getStackTrace()[2], args);
+    }
+
+    public static void debug(String stringFormat, Object... args){
+        logger.debug(buildStringFormat(stringFormat), Thread.currentThread().getStackTrace()[2], args);
+    }
+
+    public static void info(String stringFormat, Object... args){
+        logger.info(buildStringFormat(stringFormat), Thread.currentThread().getStackTrace()[2], args);
+    }
+
+    public static void warn(String stringFormat, Object... args){
+        logger.warn(buildStringFormat(stringFormat), Thread.currentThread().getStackTrace()[2], args);
+    }
+
+    private static String buildStringFormat(String stringFormat){
+        return "stackTrace: {}--" + stringFormat;
+    }
+}
